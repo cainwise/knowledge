aba062cf54040bfc311a629675e67c5625b85a51
Jeff'sMac <147154479@qq.com>
2018-03-13 00:00:33 +0800

    eslint

--- 网站前端/gulp使用/tools/.eslintrc.js
+++ 网站前端/gulp使用/tools/.eslintrc.js
@@ -20,6 +20,7 @@ module.exports = {
     'arrow-parens': 0,
     'generator-star-spacing': 0,
     'space-before-function-paren': 0,
+    'no-multi-spaces': [2, { ignoreEOLComments: true }],
     'no-console': 0,
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
 
